7f121b69b074183a8515e8ac9f05ade21d67647e
hnk1031 <civjo2dp.hu8x@gmail.com>
2016-06-05 19:53:27 +0900

    janken2

--- app/src/main/java/com/example/hnk_1031/janken/MainActivity.java
+++ app/src/main/java/com/example/hnk_1031/janken/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.hnk_1031.janken;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView player;
-    TextView cpu;
+    ImageView cpu;
     TextView result;
     int number;
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         player =(TextView)findViewById(R.id.player);
-        cpu =(TextView)findViewById(R.id.cpu);
+        cpu =(ImageView) findViewById(R.id.cpu);
         result =(TextView)findViewById(R.id.result);
     }
 
@@ -28,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
         number = random.nextInt(3);
 
         if(number ==0) {
-            cpu.setText("相手の手はグーです");
+            cpu.setImageResource(R.drawable.goo);
             result.setText("引き分けです");
         } else if(number ==1) {
-            cpu.setText("相手の手はチョキです");
+            cpu.setImageResource(R.drawable.cho);
             result.setText("あなたの勝ちです");
         } else if(number ==2) {
-            cpu.setText("相手の手はパーです");
+            cpu.setImageResource(R.drawable.paa);
             result.setText("あなたの負けです");
         }
     }
@@ -45,13 +46,13 @@ public class MainActivity extends AppCompatActivity {
         number = random.nextInt(3);
 
         if(number ==0) {
-            cpu.setText("相手の手はグーです");
+            cpu.setImageResource(R.drawable.goo);
             result.setText("あなたの負けです");
         } else if(number ==1) {
-            cpu.setText("相手の手はチョキです");
+            cpu.setImageResource(R.drawable.cho);
             result.setText("引き分けです");
         } else if(number ==2) {
-            cpu.setText("相手の手はパーです");
+            cpu.setImageResource(R.drawable.paa);
             result.setText("あなたの勝ちです");
         }
     }
@@ -62,13 +63,13 @@ public class MainActivity extends AppCompatActivity {
         number = random.nextInt(3);
 
         if(number ==0) {
-            cpu.setText("相手の手はグーです");
+            cpu.setImageResource(R.drawable.goo);
             result.setText("あなたの勝ちです");
         } else if(number ==1) {
-            cpu.setText("相手の手はチョキです");
+            cpu.setImageResource(R.drawable.cho);
             result.setText("あなたの負けです");
         } else if(number ==2) {
-            cpu.setText("相手の手はパーです");
+            cpu.setImageResource(R.drawable.paa);
             result.setText("引き分けです");
         }
 
